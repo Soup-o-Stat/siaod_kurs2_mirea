8e796442b4c4618833c3d2afaf9589fa66606fc3
Soup-o-Stat <mr.drk.knght@gmail.com>
2024-09-30 09:06:20 +0300

    5.2

--- 5_2/main.cpp
+++ 5_2/main.cpp
@@ -1,21 +1,97 @@
 #include <iostream>
 #include <fstream>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
-void create_file() {
-    ifstream file("common_file.txt");
+int global_num_of_phones=0;
+
+void mega_converter(const string &ntext_file, const string &nbinary_file) {
+    ifstream text_file(ntext_file);
+    ofstream binary_file(nbinary_file);
+
+    if (text_file.is_open() && binary_file.is_open()) {
+        string line;
+        while (getline(text_file, line)) {
+            size_t len = line.length();
+            for (int i = 0; i < len; i++) {
+                if (line[i] == ' ') {
+                    binary_file << endl;
+                }
+                else {
+                    binary_file << line[i];
+                }
+            }
+        }
+        text_file.close();
+        binary_file.close();
+        cout<<"Converted file successfully!"<<endl;
+    }
+    else {
+        cout<<"File could not be opened!"<<endl;
+    }
+};
+
+void create_file(){
+    vector<string> used_phonenumbers;
+    srand(static_cast<unsigned int>(time(0)));
+    ofstream file("common_file.txt");
     int num_of_strings=0;
     cout<<"Enter number of phone numbers: "<<endl;
     cin>>num_of_strings;
+    global_num_of_phones = num_of_strings;
     cout<<endl;
     for (int i = 0; i < num_of_strings; i++) {
-        
+        int randomNumber1 = 10 + rand() % (99 - 10 + 1);
+        int randomNumber2 = 100 + rand() % (999 - 100 + 1);
+        int randomNumber3 = 1000 + rand() % (9000 - 10 + 1);
+        string phonenumber="89"+to_string(randomNumber1)+to_string(randomNumber2)+to_string(randomNumber3);
+        if (find(used_phonenumbers.begin(), used_phonenumbers.end(), phonenumber) == used_phonenumbers.end()) {
+            used_phonenumbers.push_back(phonenumber);
+        }
+        else {
+            cout<<phonenumber<<" has been used. DETERMINATE THIS!!!"<<endl;
+            i--;
+        }
+    }
+    sort(used_phonenumbers.begin(), used_phonenumbers.end());
+    for (int i = 0; i < used_phonenumbers.size(); i++) {
+        file<<used_phonenumbers[i]<<" "<<endl;
     }
     file.close();
 }
 
+int bin_file_search() {
+    string key;
+    int phone_size=11;
+    cout<<"Enter phone number (key)"<<endl;
+    cin>>key;
+    ifstream file_for_search("common_file.bin");
+    string line;
+    int line_number;
+    while (getline(file_for_search, line)) {
+        line_number++;
+        if ((line_number-12)==(global_num_of_phones/2)) {
+            cout<<line<<endl;
+            break;
+        }
+    }
+    if (line==key) {
+        cout<<"Phone number found!"<<endl;
+        cout<<line<<endl;
+        return 0;
+    }
+    cout<<"Phone number not found!"<<endl;
+    return 0;
+}
+
 int main() {
     create_file();
+    mega_converter("common_file.txt", "common_file.bin");
+    bin_file_search();
     return 0;
 }
